1ce4fb6c45d0bc1f76310240d8afa07b6d3aa141
aldo-rd <aldo@relivedigital.com>
2022-02-15 23:29:43 +0000

    Fixed credentials.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 echo "Hello world"
 
+#############################
 # AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
-/usr/local/bin/aws --version
+#############################
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#./aws/install
+#/usr/local/bin/aws --version
 
-rm -R aws/
-rm awscliv2.zip
+#rm -R aws/
+#rm awscliv2.zip
 
+#############################
 # Dependencies
+#############################
 #npm install
 #npm install -g @aws-amplify/cli
 #npm install -g @vue/cli
