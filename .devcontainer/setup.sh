2fbe1027175682aff79f214488b491e659e493fd
aldo-rd <aldo@relivedigital.com>
2022-02-15 23:27:34 +0000

    Added aws credentials.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 echo "Hello world"
-npm install
-npm install -g @aws-amplify/cli
+
+# AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+/usr/local/bin/aws --version
+
+rm -R aws/
+rm awscliv2.zip
+
+# Dependencies
+#npm install
+#npm install -g @aws-amplify/cli
+#npm install -g @vue/cli
