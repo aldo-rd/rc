042509b3b96c25bb7ffa087d6db4af8f5f540df3
aldo-rd <aldo@relivedigital.com>
2022-02-16 00:25:34 +0000

    Update setup.sh.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
-echo "Hello world"
 
 #############################
 # AWS CLI
 #############################
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-#unzip awscliv2.zip
-#./aws/install
-#/usr/local/bin/aws --version
-
-#rm -R aws/
-#rm awscliv2.zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+rm -R aws/
+rm awscliv2.zip
 
 #############################
 # Dependencies
 #############################
-#npm install
-#npm install -g @aws-amplify/cli
-#npm install -g @vue/cli
+npm install -g @aws-amplify/cli
+npm install -g @vue/cli
+npm install
